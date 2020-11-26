a00497bd2e5c4ab4dc55d64508d4621d3820eb0e
Momcilo Krunic <krunicm@gmail.com>
2020-11-26 21:23:16 +0100

    Fixed c++ Kalman implementation.

--- kalman_multi_D.cpp
+++ kalman_multi_D.cpp
@@ -75,44 +75,31 @@ void filter(VectorXd &x, MatrixXd &P) {
 
     for (unsigned int n = 0; n < measurements.size(); ++n) {
 
-        //  # measurement update
-        //     Z = matrix([[measurements[n]]])
-        //     y = Z - (H * x) # First we compare where we think we are with what the sensor data tells us y = z - Hx'
-        //     S = H * P * H.transpose() + R
-        //     K = P * H.transpose() * S.inverse()
-        //     x = x + (K * y)
-
-        //     P = (I - (K * H)) * P
-
-        //     # prediction
-        //     x = (F * x) + u # equation does these prediction calculations for us.
-        //     P = F * P * F.transpose() # Maybe the object changed direction, accelerated or decelerated. 
-        //                               # So when we predict the position one second later, our uncertainty increases.
-
+       
         // // Kalman Filter variables
         // VectorXd x;	// object state
         // MatrixXd P;	// object covariance matrix
         // VectorXd u;	// external motion
-        // MatrixXd F; // state transition matrix
+        // MatrixXd F;  // state transition matrix
         // MatrixXd H;	// measurement matrix
         // MatrixXd R;	// measurement covariance matrix
-        // MatrixXd I; // Identity matrix
+        // MatrixXd I;  // Identity matrix
         // MatrixXd Q;	// process covariance matrix
 
+        VectorXd z = measurements[n];
+
+        // KF Measurement update step
+        VectorXd y = z - (H * x);
+        MatrixXd S = H * P * H.transpose() + R;
+        MatrixXd K = P * H.transpose() * S.inverse();
+        
+        // new state
+        x = x + K * y;
+        P = (I - K * H) * P;  
 
         // KF Prediction step
-        VectorXd x_p = F * x + u;
-        MatrixXd P_p = F * P * F.transpose() + Q;
-         
-        // KF Measurement update step
-        VectorXd z = measurements[n];
-        VectorXd y = z - (H * x_p);
-        MatrixXd S = H * P_p * H.transpose() + R;
-        MatrixXd K = P_p * H.transpose() * S.inverse();
-        // new state
-
-        x = x_p + K * y;
-        P = (I - K * H) * P_p;        
+        x = F * x + u;
+        P = F * P * F.transpose() + Q;      
         
         cout << "x=" << endl <<  x << endl;
         cout << "P=" << endl <<  P << endl;
